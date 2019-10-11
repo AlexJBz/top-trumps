b51303fe7ccc65453ac987fa8f1c880c711790f2
AlexJBz <alexjkenworthy@gmail.com>
2019-10-11 16:56:48 +0100

    Fully playable, just single player

--- js/main.js
+++ js/main.js
@@ -17,14 +17,15 @@ class Card {
         this.defense = stats.defense;
         this.evilness = stats.evilness;
         this.deception = stats.deception;
+        this.wins = 0;
     }
 
     htmlConstructor() {
         let html = '';
         html += `<div onclick="game.chooseCard(${this.id})" class="card"><h1>Trumps</h1><hr>Health: <b>${this.health}`;
         html += `</b></br>Attack: <b>${this.attack}</b></br>Defense: <b>${this.defense}</b></br>`;
-        html += `Evilness: <b>${this.evilness}</b></br>Deception: <b>${this.deception}</b>`;
-        html += '</br></div>'
+        html += `Evilness: <b>${this.evilness}</b></br>Deception: <b>${this.deception}</b></br></br>`;
+        html += `Wins: <b>${this.wins}</b></br></div>`;
         return html;
     }
 
@@ -85,14 +86,15 @@ class Card {
 
 let game = {
     player: {
+        playersChoice: true,
         deck: [],
         otherDeck: [],
         selectedCard: null,
-        selectedAttr: null
+        selectedAttr: null,
+        limboCards: null
     },
     choiceCardDeck: [],
     settings: {
-        twoPlayerMode: false,
         choiceCardLength: 30,
         maxCards: 15,
         inPlay: false
@@ -103,35 +105,56 @@ let game = {
     makeCard(id) {
         return new Card(id, {
             health: this.randomNumber(1000),
-            attack: this.randomNumber(),
-            defense: this.randomNumber(),
+            attack: this.randomNumber(500),
+            defense: this.randomNumber(750),
             evilness: this.randomNumber(10),
             deception: this.randomNumber(5)
         })
     },
     getCardById(id) {
-        if (!this.twoPlayerMode) {
-            let correctCard = null;
-            this.player.deck.forEach(card => {
-                if (card.id == id) {
-                    correctCard = card;
-                }
-            });
-            return correctCard;
+        let correctCard = null;
+        this.player.deck.forEach(card => {
+            if (card.id == id) {
+                correctCard = card;
+            }
+        });
+        return correctCard;
+    },
+    getIndexById(array, id) {
+        let correctCard = null;
+        for (let i = 0; i < array.length; i++) {
+            let card = array[i];
+            if (card.id == id) {
+                correctCard = i;
+                break;
+            } 
         }
+        return correctCard;
     },
     chooseCard(id) {
         if (!this.settings.inPlay) {
             this.player.selectedCard = this.getCardById(id);
             $('.cardRow').style.visibility = 'hidden';
-            $('.pickAtt').style.visibility = 'visible';
-            this.updateGameInfo('Pick an attribute to compare!');
+            if (this.player.playersChoice) {
+                $('.pickAtt').style.visibility = 'visible';
+                this.updateGameInfo('Pick an attribute to compare!');
+            } else {
+                let attrs = ['health', 'attack', 'defense', 'evilness', 'deception'];
+                let attr = attrs[this.randomNumber(attrs.length - 1)];
+                this.player.selectedAttr = attr;
+                this.updateGameInfo(`The chosen attribute is ${attr} | Click reveal to see who won!`);
+                $('.revealBox').style.visibility = 'visible';
+                $('.revealBox').innerHTML = this.player.selectedCard.htmlConstructor() + this.generateEmptyCard();
+                $('.revealBox').innerHTML += '</br><button onclick="game.revealTurnWinner()">Reveal</button>'
+                this.settings.inPlay = true;
+            }
         }
     },
     chooseAttribute(attr) {
-        game.player.selectedAttr = attr;
+        this.player.selectedAttr = attr;
         $('.pickAtt').style.visibility = 'hidden';
         this.updateGameInfo('Click reveal to see who won!');
+        $('.revealBox').style.visibility = 'visible';
         $('.revealBox').innerHTML = this.player.selectedCard.htmlConstructor() + this.generateEmptyCard();
         $('.revealBox').innerHTML += '</br><button onclick="game.revealTurnWinner()">Reveal</button>'
         this.settings.inPlay = true;
@@ -149,21 +172,67 @@ let game = {
         this.player.otherDeck = deck;
     },
     revealTurnWinner() {
-        let otherCard = this.player.otherDeck[this.randomNumber(this.player.otherDeck.length - 1)];
+        let indexOfOtherCard = this.randomNumber(this.player.otherDeck.length - 1);
+        if (this.player.otherDeck.length == 1) {
+            indexOfOtherCard = 0;
+        }
+        let otherCard = this.player.otherDeck[indexOfOtherCard];
         $('.revealBox').innerHTML = this.player.selectedCard.htmlConstructor() + otherCard.htmlConstructor();
+        $('.revealBox').innerHTML += '</br><button onclick="game.nextTurn()">Continue</button>';
         let state = this.player.selectedCard.compare(otherCard);
         if (state === true) {
+            this.player.playersChoice = true;
             $('.gameInfo').innerHTML = 'Player 1 won this turn!';
+            this.player.selectedCard.wins++;
+            this.player.deck.push(otherCard);
+            this.player.otherDeck.splice(indexOfOtherCard, 1);
+            if (this.player.limboCards) {
+                this.addLimboCards(this.player.deck);
+            }
         } else if (state === false) {
             $('.gameInfo').innerHTML = 'Player 2 won this turn!';
+            this.player.playersChoice = false;
+            otherCard.wins++;
+            this.player.otherDeck.push(this.player.selectedCard);
+            this.player.deck.splice(this.getIndexById(this.player.deck, this.player.selectedCard.id), 1);
+            if (this.player.limboCards) {
+                this.addLimboCards(this.player.otherDeck);
+            }
         } else {
-            // Limbo state
+            this.updateGameInfo('There was a tie! Both cards have entered a limbo state! Next winner takes all!');
+            this.player.otherDeck.splice(indexOfOtherCard, 1);
+            this.player.deck.splice(this.getIndexById(this.player.deck, this.player.selectedCard.id), 1);
+            this.player.limboCards = state;
         }
+        this.player.selectedCard = null;
+        this.player.selectedAttr = null;
+    },
+    addLimboCards(array) {
+        this.player.limboCards.forEach(card => {
+            array.push(card);
+        });
+        this.player.limboCards = null;
+    },
+    nextTurn() {
+        $('.revealBox').innerHTML = '';
+        $('.revealBox').style.visibility = 'hidden';
+        if (this.player.deck.length == 0 || this.player.otherDeck.length == 0) {
+            if(this.player.otherDeck.length == 0) {
+                this.updateGameInfo('Player 1 has won the game!');
+            } else {
+                this.updateGameInfo('Player 2 has won the game!');
+            }
+        } else {
+            this.settings.inPlay = false;
+            this.updateGameInfo('Pick another card!');
+        }
+        this.displayPlayerCards(this.player.deck);
+        $('.cardRow').style.visibility = 'visible';
     },
     generateEmptyCard() {
         let html = '';
         html += `<div class="card"><h1>Trumps</h1><hr>Health: <b>?</b></br>Attack: <b>?</b></br>`;
-        html += `Defense: <b>?</b></br>Evilness: <b>?</b></br>Deception: <b>?</b>`;
+        html += `Defense: <b>?</b></br>Evilness: <b>?</b></br>Deception: <b>?</b></br></br>`;
         html += '</br></div>'
         return html;
     },
